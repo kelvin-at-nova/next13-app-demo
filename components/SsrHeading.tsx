5bd294b2305130d32d5c181375ece5d9c531363a
kelvin-at-nova <kelvinli@novaentertainment.com.au>
2023-03-21 09:33:40 +1100

    text alignment

--- components/SsrHeading.tsx
+++ components/SsrHeading.tsx
@@ -3,10 +3,10 @@ import { getBrand } from "@/preloaders/getBrand";
 
 const SsrHeading = ({ name, image, acn }: Company) => {
   return (
-    <div style={{ display: "flex", gap: "20px" }}>
+    <div style={{ display: "flex", gap: "20px", alignItems: "center" }}>
       <img src={image} height={32} width={32} />
-      <h3>{name}</h3>
-      <h4>ACN: {acn}</h4>
+      <h1>{name}</h1>
+      <h2>ACN: {acn}</h2>
     </div>
   );
 };
